6a5261d198db9b3715e036c29dfaa6a9372839c1
subash <155135026+Subxsh@users.noreply.github.com>
2025-09-18 10:12:01 +0530

    [485] [Time Beats: 95.36%] [Memory Beats: 79.15%] - LeetPush

---
+++ Max-Consecutive-Ones.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        
+        int maxCount = 0;
+        int count = 0;
+        
+        for(int num: nums){
+            if(num == 1){
+                count++;
+            }else{
+                count = 0;
+            }
+            
+            if(maxCount < count){
+                maxCount = count;
+            }
+        }
+        
+        return maxCount;
+    }
+}

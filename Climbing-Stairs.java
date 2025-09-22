26fffe3593277606e66b85489c1b2618fb312df0
subash <155135026+Subxsh@users.noreply.github.com>
2025-09-22 11:16:21 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 10.91%] - LeetPush

--- Climbing-Stairs.java
+++ Climbing-Stairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         
-    if (n == 1) return 1;
+    if (n == 1||n==2) return n;
     
     int a = 1, b = 2;
     for (int i = 3; i <= n; i++) {
